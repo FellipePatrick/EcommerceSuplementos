b85f5a71c77a52f756aaa836c43833b36a85bc1a
FellipePatrick <fellipe.brito@ifrn.edu.br>
2024-07-25 13:51:35 -0300

    primeira versão do codigo

---
+++ EcommercApplication/src/main/java/cog/com/EcommercApplication/EcommercApplication.java
@@ -0,0 +1,13 @@
+package cog.com.EcommercApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercApplication.class, args);
+	}
+
+}
---
+++ EcommercApplication/src/test/java/cog/com/EcommercApplication/EcommercApplicationTests.java
@@ -0,0 +1,13 @@
+package cog.com.EcommercApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

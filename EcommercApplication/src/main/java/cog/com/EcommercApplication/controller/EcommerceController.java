50fc6cb94c2c3991f7ac167fdee8f1c602f36e83
FellipePatrick <fellipe.brito@ifrn.edu.br>
2024-07-25 16:22:24 -0300

    implementação de algumas paginas

--- EcommercApplication/src/main/java/cog/com/EcommercApplication/controller/EcommerceController.java
+++ EcommercApplication/src/main/java/cog/com/EcommercApplication/controller/EcommerceController.java
@@ -8,9 +8,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class EcommerceController {
 
-    @GetMapping({"/", "/index"})
+    @GetMapping({"/"})
     public String index(){
         return "index";
+    }
+    
+    @GetMapping({"/cadastroSuplemento"})
+    public String doCadastroSuplemento(){
+        return "cadastroSuplementos";
+    }
+
+    @GetMapping({"/editarSuplemento"})
+    public String doEditarSuplemento(){
+        return "editarSuplementos";
+    }
+
+    @GetMapping({"/cadastro"})
+    public String doCadastro(){
+        return "cadastroUsuario";
+    }
+
+    @GetMapping({"/login"})
+    public String doLogin(){
+        return "login";
     }
     
     
--- EcommercApplication/src/main/java/cog/com/EcommercApplication/domain/Suplementos.java
+++
@@ -1,9 +0,0 @@
-package cog.com.EcommercApplication.domain;
-import jakarta.persistence.Entity;
-import lombok.Data;
-
-@Entity
-@Data
-public class Suplementos{
-
-}
